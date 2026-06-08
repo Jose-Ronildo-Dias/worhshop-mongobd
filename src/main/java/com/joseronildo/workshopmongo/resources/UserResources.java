4a9af7201b1b01997a1204a2a879d13576446ce4
José Ronildo <joseronidias@outlook.com>
2026-06-08 15:46:43 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/joseronildo/workshopmongo/resources/UserResources.java
+++ src/main/java/com/joseronildo/workshopmongo/resources/UserResources.java
@@ -1,5 +1,6 @@
 package com.joseronildo.workshopmongo.resources;
 
+import com.joseronildo.workshopmongo.domain.Post;
 import com.joseronildo.workshopmongo.domain.User;
 import com.joseronildo.workshopmongo.dto.UserDTO;
 import com.joseronildo.workshopmongo.services.UserService;
@@ -54,5 +55,10 @@ public class UserResources {
         obj.setId(id);
         obj = service.update(obj);
        return ResponseEntity.noContent().build();
+    }
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
